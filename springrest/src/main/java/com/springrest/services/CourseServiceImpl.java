50cecf769f29ba3513009e5d134b70a1f26ed0c4
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-03-15 14:22:19 +0530

    First REST API Application

---
+++ springrest/src/main/java/com/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,63 @@
+package com.springrest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.springrest.entities.Course;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+	List<Course> list;
+
+	public CourseServiceImpl() {
+		list = new ArrayList<>();
+		list.add(new Course(101, "Core java", "This is Core Java Course"));
+		list.add(new Course(102, "Spring", "This is Spring Course"));
+
+	}
+
+	@Override
+	public List<Course> getCourses() {
+
+		return list;
+	}
+
+	@Override
+	public Course getCourse(long courseId) {
+		Course c = null;
+		for (Course course : list) {
+			if (course.getId() == courseId) {
+				c = course;
+				break;
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		list.add(course);
+		return course;
+	}
+
+	@Override
+	public Course updateCourse(Course course) {
+		list.forEach(e -> {
+			if (e.getId() == course.getId()) {
+				e.setTitle(course.getTitle());
+				e.setDescription(course.getDescription());
+			}
+		});
+		return course;
+	}
+	
+	@Override
+	public void deleteCourse(long parseLong) {
+	 list=this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
+	}
+
+}
