6a75996896175e83c02745b1bbeba09134e3449b
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-04-09 15:56:11 +0530

    Student Course files

---
+++ Day6_Student_Course_Enrollment_System_using_Query/src/main/java/com/nit/Day6StudentCourseEnrollmentSystemUsingQueryApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day6StudentCourseEnrollmentSystemUsingQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day6StudentCourseEnrollmentSystemUsingQueryApplication.class, args);
+	}
+
+}
