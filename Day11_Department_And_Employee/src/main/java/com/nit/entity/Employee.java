980400592d220045316057aed4c7491449bb2a3f
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-04-18 22:38:42 +0530

    emp

---
+++ Day11_Department_And_Employee/src/main/java/com/nit/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.nit.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee {
+	
+	Long eid;
+	String name;
+
+}
