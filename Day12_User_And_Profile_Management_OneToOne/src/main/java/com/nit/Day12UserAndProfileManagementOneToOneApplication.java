8c2375eb4c3fe46943b117e22bc96ddf099824e5
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-04-21 12:38:39 +0530

    Day 12 User And Profile One to ONe

---
+++ Day12_User_And_Profile_Management_OneToOne/src/main/java/com/nit/Day12UserAndProfileManagementOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12UserAndProfileManagementOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12UserAndProfileManagementOneToOneApplication.class, args);
+	}
+
+}
