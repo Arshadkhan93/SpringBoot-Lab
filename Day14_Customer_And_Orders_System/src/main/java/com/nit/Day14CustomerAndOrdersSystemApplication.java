9ce98142138fb6f12e00aa34a27523c4ac0b244b
Arshadkhan93 <arshadkhan10062005@gmail.com>
2026-04-30 23:13:59 +0530

    Day 14 Customer Order System

---
+++ Day14_Customer_And_Orders_System/src/main/java/com/nit/Day14CustomerAndOrdersSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day14CustomerAndOrdersSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day14CustomerAndOrdersSystemApplication.class, args);
+	}
+
+}
